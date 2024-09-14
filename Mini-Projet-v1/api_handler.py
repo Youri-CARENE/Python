49ff58ac636c13b562e5519811411425ce88dd46
Yo17 <youri.carene@gmail.com>
2024-09-14 23:31:39 +0200

    module pour les appels à l'API

---
+++ Mini-Projet-v1/api_handler.py
@@ -0,0 +1,9 @@
+import requests
+
+def fetch_data(api_url):
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(f"API request failed: {e}")
